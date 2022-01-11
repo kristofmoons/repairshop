d49b9dcea8502f280a3ffe7a29275b944b157e13
kristofmoons <r0696321@student.thomasmore.be>
2022-01-11 21:44:11 +0100

    gui kan klant toevoegen

--- src/repairshop/RepShopTestGui.java
+++ src/repairshop/RepShopTestGui.java
@@ -2,12 +2,17 @@ package repairshop;
 
 public class RepShopTestGui {
     public static void main(String[] args) {
-        Klant k1 = new Klant("Jos","De Wolf", "04/52455313" ,"JosDeWold@email.com");
-        Klant k2 = new Klant("Jes","De vis", "04/52455313" ,"JesDekat@email.com");
+        Repairshop r1 = new Repairshop();
+        Klant k1 = new Klant("De Wolf","Jos", "04/52455313" ,"JosDeWold@email.com");
+        Klant k2 = new Klant("De vis","Jes", "04/52455313" ,"JesDekat@email.com");
+        Bedrijf b1 = new Bedrijf("De Kat","Jef","04/2369745","bedr@opn&co.com","opn&co");
+        Item i1 = new Item("horloge", "geeft tijd niet goed aan",50, "bezig met reparatie");
+        Item i2 = new Item("Jas","mouw gescheurd",100,"bezig met reparatie");
+        Item i3 = new Item("autozetels","versleten(gescheurd, krakken,...)",500,"in werking");
 
         RepshopGui repshopGui = new RepshopGui();
         KlantPanel klantPanel = new KlantPanel(k1);
-        klantPanel.run();
-//        repshopGui.run();
+//        klantPanel.run();
+        repshopGui.run();
     }
 }
--- src/repairshop/RepshopGui.java
+++ src/repairshop/RepshopGui.java
@@ -1,11 +1,48 @@
 package repairshop;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RepshopGui {
     private JPanel paneelRepShop;
-    private JButton button1;
+    private JButton nieuweKlantButton;
+    private JButton button2;
+    private JTextField telefoontextfield;
+    private JTextField emailtextfield;
+    private JTextField naamtextfield;
+    private JTextField voornaamtextfield;
     private JList list1;
+    private Repairshop repairshop = new Repairshop();
+    private DefaultListModel<String> lijstPersonen = new DefaultListModel<>();
+
+
+    public RepshopGui() {
+        nieuweKlantButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String naam = naamtextfield.getText();
+                String voornaam = voornaamtextfield.getText();
+                String telefoon = telefoontextfield.getText();
+                String email = emailtextfield.getText();
+                Klant klant = new Klant(naam, voornaam, telefoon,email);
+                voegToe(klant);
+
+            }
+        });
+    }
+
+    public void voegToe(Klant klant){
+        repairshop.voegKlantToe(klant);
+        lijstPersonen.removeAllElements();
+        for (Klant k : repairshop.getContacten().values()){
+            System.out.println(k);
+            lijstPersonen.addElement(k.getNaam()+" "+k.getVoornaam());
+        }
+
+
+
+    }
 
     public void run() {
         JFrame frame = new JFrame();
